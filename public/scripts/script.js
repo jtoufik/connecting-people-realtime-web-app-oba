821618d26908eb85021d9da38a72c30390a9f67b
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-05-24 10:47:58 +0200

    dialog toegevoegd

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -43,10 +43,12 @@ const openButton = document.querySelector("[data-open-modal]")
 const closeButton = document.querySelector("[data-close-modal]")
 const modal = document.querySelector("[data-modal]")
 
-openButton.addEventListener("click"), () =>{
-    modal.showModal()
-}
+openButton.addEventListener("click", () =>{
+    modal.showModal() 
+    
+    console.log(openButton)
+})
 
-closeButton.addEventListener("click"), () =>{
+closeButton.addEventListener("click", () =>{
     modal.close()
-}
+})
